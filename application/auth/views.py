ff422394286b0724d01fa170812ed455f7d3d650
Annukka Närhi <annukka.narhi@gmail.com>
2020-02-06 12:42:11 +0200

    Rekisteröityminen

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,26 +1,30 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
-    if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+	if request.method == "GET":
+		return render_template("auth/loginform.html", form = LoginForm())
 
-    form = LoginForm(request.form)
+	if request.form.get('login'):
+		form = LoginForm(request.form)
     # mahdolliset validoinnit
 
-    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
-    if not user:
-        return render_template("auth/loginform.html", form = form,
-                               error = "No such username or password")
+		user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+		if not user:
+			return render_template("auth/loginform.html", form = form,
+                               error = "No such username or password. Register a new user or check spelling.")
+	else:
+        	user=User(request.form.get("username"),request.form.get("username"),request.form.get("password"))
+        	db.session().add(user)
+        	db.session().commit()
 
-
-    login_user(user)
-    return redirect(url_for("index"))
+	login_user(user)
+	return redirect(url_for("index"))
 
 @app.route("/auth/logout")
 def auth_logout():
