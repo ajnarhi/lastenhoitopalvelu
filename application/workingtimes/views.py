73148fe6f7711fb456518013bbe392c90790c86e
Annukka Närhi <annukka.narhi@gmail.com>
2020-02-06 21:21:58 +0200

    Lastenhoitajan varausnappi toimii

--- application/workingtimes/views.py
+++ application/workingtimes/views.py
@@ -5,6 +5,12 @@ from application import app, db
 from application.workingtimes.models import Workingtimes
 from application.workingtimes.forms import WorkingtimesForm
 
+@app.route("/workingtimes/<id>/")
+def workingtimes_list(id):
+    workingtimes=Workingtimes.query.filter_by(nanny_id=id)
+    return render_template("workingtimes/listofworkingtimes.html", workingtimes=workingtimes)
+
+
 @app.route("/workingtimes/new/")
 def workingtimes_form():
     return render_template("workingtimes/new.html", form = WorkingtimesForm())
@@ -17,3 +23,13 @@ def workingtimes_create():
     db.session().commit()
   
     return "hello world!"
+
+@app.route("/workingtimes/reserve/<id>/", methods=["POST"])
+def workingtimes_set_reserved(id):
+
+    w = Workingtimes.query.get(id)
+    w.reserved = True
+    db.session().commit()
+  
+    return redirect(url_for("workingtimes_list", id=w.nanny_id)) #kun klikataan aika w varatuksi, palataan ajan w omaavan nannyn vapaana oleviin aikoihin
+
