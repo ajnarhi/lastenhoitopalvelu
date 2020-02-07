308ec7f0e188da639595098ee9c222730f44139f
Annukka Närhi <annukka.narhi@gmail.com>
2020-02-07 15:29:44 +0200

    Ulkoasun viilausta

--- application/nannies/models.py
+++ application/nannies/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class Nanny(Base):
 
     name = db.Column(db.String(144), nullable=False)
@@ -17,3 +19,17 @@ class Nanny(Base):
         self.age = age
         self.phonenumber = phonenumber
 
+    @staticmethod
+    def find_nannies_with_workingtimes():
+        stmt = text("SELECT Nanny.id, Nanny.name FROM Nanny"
+                    " LEFT JOIN Workingtimes ON workingtimes.nanny_id = nanny.id"
+                    " WHERE NOT Workingtimes.reserved"
+                    " GROUP BY Nanny.id"
+                    " HAVING COUNT(Workingtimes.id) > 0")
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
+
+        return response
--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from application import app
+from application.nannies.models import Nanny
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", has_workingtimes=Nanny.find_nannies_with_workingtimes())
